f6bfda6c2a94c8025d371c4d83a538ebff55d617
Luke <luke.weston@cyberus-technology.de>
2026-06-19 13:27:13 +0200

    networking

--- pi/configuration.nix
+++ pi/configuration.nix
@@ -92,23 +92,7 @@
     ];
   };
 
-  # networking.networkmanager.enable = true;
-  # networking.useDHCP = lib.mkDefault true;
-
-  networking = {
-    interfaces.end0 = {
-      ipv4.addresses = [{
-        address = "192.0.2.2";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = {
-      address = "192.0.2.1";
-      interface = "ens3";
-    };
-  };
-
-
+  networking.useDHCP = lib.mkDefault true;
 
   time.timeZone = "Europe/Berlin";
 
